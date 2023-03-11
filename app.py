ea76750de7b8b9ce0ae0cb56b556383584ecfb3f
digimori <hanaorikuu@gmail.com>
2023-03-11 19:19:53 +0000

    Added cards into the collapsible and added a for loop to iterate through the database to display entry onto its own card

--- app.py
+++ app.py
@@ -86,12 +86,13 @@ def login():
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    books = list(mongo.db.booksread.find())
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
     if session["user"]:
-        return render_template("profile.html", username=username)
+        return render_template("profile.html", username=username, booksread=books)
 
     return redirect(url_for("login"))
 
