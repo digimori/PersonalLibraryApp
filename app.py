856faf39073ab1c3cd72e1a3cd546cce7ac26e74
digimori <hanaorikuu@gmail.com>
2023-03-12 14:18:50 +0000

    Test modal added for edit buttons on book entries in profile.html, added display for books to be read from the database

--- app.py
+++ app.py
@@ -83,12 +83,16 @@ def login():
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     books = list(mongo.db.booksread.find())
+    unread = list(mongo.db.bookstoberead.find())
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
     if session["user"]:
-        return render_template("profile.html", username=username, booksread=books)
+        return render_template(
+            "profile.html", 
+            username=username, booksread=books, bookstoberead=unread
+            )
 
     return redirect(url_for("login"))
 
