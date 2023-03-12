74fd9fac979a7671567d20057838b14dd32db919
digimori <hanaorikuu@gmail.com>
2023-03-12 11:41:00 +0000

    Some UI changes, rounded corners on welcome message and collapsibles. Edited landing page index.html  to not have a nav bar until logged in.

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def register():
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            flash("Username already exists")
+            flash("Username already exists, please choose a different name")
             return redirect(url_for("register"))
 
         register = {
@@ -58,12 +58,10 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # check if username exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            # ensure hashed password matches user input
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                         session["user"] = request.form.get("username").lower()
@@ -72,12 +70,10 @@ def login():
                         return redirect(url_for(
                             "profile", username=session["user"]))
             else:
-                # invalid password match
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
         else:
-            # username doesn't exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
 
